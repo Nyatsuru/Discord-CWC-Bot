5bc133a2bf4f3fb1735108de925ddd3110ceec7d
Julius Noack <me@jnoack.de>
2022-10-30 00:27:40 +0200

    add inventory description for better visibillity

--- commands/slash/Drops/showInventory.js
+++ commands/slash/Drops/showInventory.js
@@ -2,7 +2,7 @@ const { EmbedBuilder } = require('discord.js');
 const Playerdrop = require('../../../schemas/playerdrop');
 
 module.exports = {
-  name: "showInventory", // Name of command
+  name: "showinventory", // Name of command
   description: "shows your drops",
   type: 1, // Command type
   options: [], // Command options
@@ -11,17 +11,31 @@ module.exports = {
     DEFAULT_MEMBER_PERMISSIONS: "" // User permissions needed
   },
   run: async function(client, interaction, config, db) {
-    const playerdrops = await Playerdrop.find({ player: interaction.player.id }).exec();
+    const playerdrops = await Playerdrop.find({ player: interaction.user.id }).populate('drop').exec();
+    const sortedPlayerdrops = {
+      unique: playerdrops.filter(playerdrop => playerdrop.drop.rarity === 'unique'),
+      legendary: playerdrops.filter(playerdrop => playerdrop.drop.rarity === 'legendary'),
+      epic: playerdrops.filter(playerdrop => playerdrop.drop.rarity === 'epic'),
+      rare: playerdrops.filter(playerdrop => playerdrop.drop.rarity === 'rare'),
+      uncommon: playerdrops.filter(playerdrop => playerdrop.drop.rarity === 'uncommon'),
+      common: playerdrops.filter(playerdrop => playerdrop.drop.rarity === 'common'),
+    }
 
-    playerdrops.forEach(playerdrop => {
-      interaction.reply({
-        embeds: [
-          new EmbedBuilder()
-              .setTitle(playerdrop.drop.name)
-              .setAuthor({ name: playerdrop.drop.rarity })
-              .setImage(playerdrop.drop.image)
-        ],
-      });
-    })
+    let description = '';
+    for (const rarity in sortedPlayerdrops) {
+      description += `\n\n ${rarity}:`
+
+      sortedPlayerdrops[rarity].forEach(playerdrop => {
+        description += `\n ${playerdrop.drop.name}`
+      })
+    }
+
+    interaction.reply({
+      embeds: [
+        new EmbedBuilder()
+            .setTitle('Inventory')
+            .setDescription(description)
+      ],
+    });
   },
 };
